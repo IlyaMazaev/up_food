0550a6f9e16c6b4a8cb54a4bb6ee5f5804f8ba72
Ilya9291 <74893784+Ilya9291@users.noreply.github.com>
2022-01-16 00:43:57 +0300

    added procfile to run on heroku

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ from requests import get, post
 from json import dumps
 
 
-params_2 = {'search_request': 'курин'}
+params_search = {'search_request': 'курин'}
 
-print(dumps(get('http://localhost:5000/api/recipes/search', params=params_2).json(), indent=4, ensure_ascii=False))
+# для поиска:
+print(dumps(get('http://localhost:5000/api/recipes/search', params=params_search).json(), indent=4, ensure_ascii=False))
 
+# рецепт по id
 print(dumps(get('http://localhost:5000/api/recipes/5').json(), indent=4, ensure_ascii=False))
+
+# все рецепты
+print(dumps(get('http://localhost:5000/api/recipes').json(), indent=4, ensure_ascii=False))
