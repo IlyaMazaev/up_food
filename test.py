1ede0261950faea587e556d051b22cd3bfb81f37
Ilya9291 <74893784+Ilya9291@users.noreply.github.com>
2022-01-25 00:48:20 +0300

    added nutrition program orm classes, functions, api

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import base64
 from json import dumps
+from requests.auth import HTTPBasicAuth
 
 from requests import get
 
 
+# генерация строки user:pass в base 64 кодировке
 sample_string = "api_user:super_secret_password"
 sample_string_bytes = sample_string.encode("ascii")
 
@@ -12,20 +14,40 @@ base64_password = base64_bytes.decode("ascii")
 
 print(f"Encoded base 64 password: {base64_password}")
 
-params_search = {f'Authorization': f'Basic {base64_password}',
-                 'search_request': 'курин'}
 
+# для поиска
+params_search = {'search_request': 'курин'}
+
+# для авторизации по заголовкам
 only_auth_headers = {'Authorization': f'Basic {base64_password}'}
 
+# для авторизации через механизмы request
+basic = HTTPBasicAuth('api_user', 'super_secret_password')
 
+# авторизация по заголовкам
 # для поиска:
-print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes/search', params=params_search).json(), indent=4, ensure_ascii=False))
+print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes/search', headers=only_auth_headers, params=params_search).json(), indent=4, ensure_ascii=False))
 
 # рецепт по id
 print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes/5', headers=only_auth_headers).json(), indent=4, ensure_ascii=False))
 
 # все рецепты
 print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes', headers=only_auth_headers).json(), indent=4, ensure_ascii=False))
+
+
+
+
+# авторизация через requests
+# для поиска:
+print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes/search', auth=basic, params=params_search).json(), indent=4, ensure_ascii=False))
+
+
+# рецепт по id
+print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes/5', auth=basic).json(), indent=4, ensure_ascii=False))
+
+# все рецепты
+print(dumps(get('https://recipes-db-api.herokuapp.com/api/recipes', auth=basic).json(), indent=4, ensure_ascii=False))
+
 
 '''
 все адреса
