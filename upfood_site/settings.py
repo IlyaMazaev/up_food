f2f8ab5e1e2487d7a88abc575187fa6e1eacd192
GreenApple <ivan.syev6@gmail.com>
2022-03-25 10:32:47 +0300

    Debug on

--- upfood_site/settings.py
+++ upfood_site/settings.py
@@ -26,7 +26,7 @@ with open('secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['takecook.ru', '.takecook.ru']
 
